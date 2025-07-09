1a0d4421fb9904674d4f90e86272022d52b7b275
claryfigs <clara.figueiredo@gmail.com>
2025-07-09 13:47:34 -0300

    modal view item client

--- src/components/modal-client-item/modal-client-item.tsx
+++ src/components/modal-client-item/modal-client-item.tsx
@@ -3,14 +3,27 @@ import './modal-client-item.css';
 import CounterItems from '../counter-items/counter-items';
 import Button from '../Button/Button';
 import CloseIcon from '../../assets/close-icon.png';
-import FoodIcon from '../../assets/sanduiche.jpg';
 
+type Item = {
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+  categories: string[];
+  image: string;
+  is_available: boolean;
+};
 
 type ModalClientItemProps = {
   onClose: () => void;
+  item: Item;
 };
 
-const ModalClientItem: React.FC<ModalClientItemProps> = ({ onClose }) => {
+const ModalClientItem: React.FC<ModalClientItemProps> = ({ onClose, item }) => {
+  const imageUrl = item.image?.startsWith('http')
+    ? item.image
+    : `http://localhost:8000${item.image}`;
+
   return (
     <div className="modal-client-item-overlay">
       <div className="modal-client-item-content">
@@ -26,11 +39,12 @@ const ModalClientItem: React.FC<ModalClientItemProps> = ({ onClose }) => {
         </div>
 
         <div className='modal-client-item-imagespace'>
-          <div className='modal-client-item-image'
+          <div
+            className='modal-client-item-image'
             style={{
-            backgroundImage: `url(${FoodIcon})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+              backgroundImage: `url(${imageUrl})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
             }}
           ></div>
         </div>
@@ -38,17 +52,17 @@ const ModalClientItem: React.FC<ModalClientItemProps> = ({ onClose }) => {
         <div className='modal-client-item-infos'>
           <div className='modal-client-item-infos2'>
             <p className='modal-client-item-title'>Nome do item:</p>
-            <p className='modal-client-item-subtitle'>Presunto</p>
+            <p className='modal-client-item-subtitle'>{item.name}</p>
           </div>
 
           <div className='modal-client-item-infos2'>
             <p className='modal-client-item-title'>Categoria:</p>
-            <p className='modal-client-item-subtitle'>Sanduíches</p>
+            <p className='modal-client-item-subtitle'>{item.categories.join(', ')}</p>
           </div>
 
           <p className='modal-client-item-title'>Descrição:</p>
           <div className='modal-client-item-description'>
-            <p>Descrição do item</p>
+            <p>{item.description}</p>
           </div>
 
           <p className='modal-client-item-title'>Quantidade desejada:</p>
@@ -56,7 +70,7 @@ const ModalClientItem: React.FC<ModalClientItemProps> = ({ onClose }) => {
 
           <div className='modal-client-item-infos2'>
             <p className='modal-client-item-title'>Valor total:</p>
-            <p className='modal-client-item-price'>R$ 20,00</p>
+            <p className='modal-client-item-price'>R$ {Number(item.price).toFixed(2)}</p>
           </div>
 
           <div className='modal-client-item-buttonspace'>
